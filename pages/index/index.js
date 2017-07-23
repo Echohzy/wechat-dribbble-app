eb2f88c8b78a847683025d5aa4b792c6b25dea5e
Echohzy <1820123540@qq.com>
2017-07-23 22:14:07 +0800

    add fetch

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,9 @@ var fetchData = require("../../utils/fetch.js").fetchData;
 var app = getApp()
 Page({
   data: {
-    shots: []
+    shots: [],
+    current_page: 0,
+    requesting: false
   },
   //事件处理函数
   bindViewTap: function(event) {
@@ -12,12 +14,17 @@ Page({
       url: '../detail/detail?shot_id=' + event.target.dataset.id
     })
   },
-  fetchShotData: function(type){
-    var that = this;
+  fetchShotData: function(){
+    let {data} = this;
+    let that = this;
+    that.setData({requesting: true});
     fetchData({
-      url: "/shots?per_page=15&page=1&list="+type,
+      url: "/shots?per_page=15&page="+(data.current_page+1),
       success: function(res){
-        that.setData({shots: res.data});
+        that.setData({ shots: data.shots.concat(res.data), current_page: data.current_page + 1,requesting: false});
+      },
+      error: function(){
+        that.setData({requesting: false});
       }
     });
   },
@@ -25,5 +32,11 @@ Page({
     //调用应用实例的方法获取全局数据
     this.fetchShotData();
     
+  },
+  onReachBottom:function(){
+    if(this.data.requesting){
+      return;
+    }
+    this.fetchShotData();
   }
 });
